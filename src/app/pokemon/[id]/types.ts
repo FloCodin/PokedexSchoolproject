247e1045a24d106e06ec8275f60e34154bd8cac7
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-07 13:41:59 +0200

    Add TypeScript interfaces for Pokemon data structure
    Introduce comprehensive TypeScript interfaces to define the structure for Pokemon data, including abilities, moves, stats, types, and evolution stages. These interfaces enhance type safety and provide a clear contract for handling Pokemon-related data within the application.

---
+++ src/app/pokemon/[id]/types.ts
@@ -0,0 +1,59 @@
+// app/pokemon/[id]/types.ts
+
+export interface NamedAPIResource {
+    name: string;
+    url: string;
+}
+
+export interface PokemonStat {
+    base_stat: number;
+    stat: NamedAPIResource;
+}
+
+export interface PokemonAbility {
+    ability: NamedAPIResource;
+}
+
+export interface PokemonMove {
+    move: {
+        name: string;
+        type: string;
+        power: number | string;
+        accuracy: number | string;
+        pp: number;
+        priority: number;
+        damageClass: string;
+        effect: string;
+    };
+}
+
+export interface PokemonType {
+    type: NamedAPIResource;
+}
+
+export interface Pokemon {
+    name: string;
+    height: number;
+    weight: number;
+    abilities: PokemonAbility[];
+    moves: PokemonMove[];
+    stats: PokemonStat[];
+    types: PokemonType[];
+    sprites: {
+        front_default: string;
+        front_shiny: string;
+    };
+    captureRate: number;
+    evolutionChain: EvolutionStage[];
+    baseStatTotal: number;
+}
+
+export interface EvolutionStage {
+    name: string;
+    sprite: string;
+    evolvesTo: {
+        name: string;
+        sprite: string;
+        condition: string;
+    }[];
+}
