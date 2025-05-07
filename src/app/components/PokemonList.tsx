441bc8bc56f44b73c59fcfba0d3a66cce2328379
FloCodin <137909251+FloCodin@users.noreply.github.com>
2025-05-07 14:32:21 +0200

    Add PokemonCard component to display Pokémon details
    This commit introduces a reusable React component, `PokemonCard`, for rendering Pokémon details, including name, types, and sprite. It uses dynamic styling for type badges and provides a fallback for missing images.

---
+++ src/app/components/PokemonList.tsx
@@ -0,0 +1,36 @@
+// components/PokemonList.tsx
+'use client';
+
+import React from 'react';
+import PokemonCard from './PokemonCard';
+
+interface Pokemon {
+    id: number;
+    name: string;
+    types: string[];
+    sprites: {
+        front_default: string | null;
+    };
+}
+
+interface PokemonListProps {
+    pokemons: Pokemon[];
+}
+
+const PokemonList: React.FC<PokemonListProps> = ({ pokemons }) => {
+    return (
+        <ul className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-6">
+            {pokemons.map((pokemon) => (
+                <PokemonCard
+                    key={pokemon.id}
+                    id={pokemon.id}
+                    name={pokemon.name}
+                    types={pokemon.types}
+                    sprite={pokemon.sprites.front_default}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default PokemonList;
